9bfa8f82c248481adca5b774da325a9112b07e69
Alena <magii.alena@yandex.ru>
2023-07-05 17:24:40 +0300

    Решение 23 задачи

--- DZ3.cs
+++ DZ3.cs
@@ -20,20 +20,27 @@
 // }
 
 // 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
-Console.Write("Введите координату по оси оХ для первой точки: ");
-int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату по оси оY для первой точки: ");
-int y1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату по оси оZ для первой точки: ");
-int z1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату по оси оХ для второй точки: ");
-int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату по оси оY для второй точки: ");
-int y2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите координату по оси оZ для второй точки: ");
-int z2 = Convert.ToInt32(Console.ReadLine());
-double distance = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
-Console.WriteLine($"Расстояние: {distance}");
-
+// Console.Write("Введите координату по оси оХ для первой точки: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оY для первой точки: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оZ для первой точки: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оХ для второй точки: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оY для второй точки: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату по оси оZ для второй точки: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// double distance = Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2)), 2);
+// Console.WriteLine($"Расстояние: {distance}");
 
 // 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+int start = 1;
+Console.Write("Введите число: ");
+int end = Convert.ToInt32(Console.ReadLine());
+while (start <= end)
+{
+    Console.Write(Math.Pow(start, 3) + " ");
+    start++;
+}
